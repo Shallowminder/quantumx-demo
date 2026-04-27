fa7d2992631822640ed4452511ab908e652e1114
Shallowminder <1450963812@qq.com>
2026-04-27 16:26:45 +0800

    feat: initialize visual style before render

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./style-presets.css";
+
+const storedStylePreference = window.localStorage.getItem("quantumx-style-preference");
+document.documentElement.dataset.style =
+  storedStylePreference === "cold-white" ||
+  storedStylePreference === "moonlight" ||
+  storedStylePreference === "glass" ||
+  storedStylePreference === "dark-first"
+    ? storedStylePreference
+    : "moonlight";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
